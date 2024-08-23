3e65a61bee7b0127ec7fc595b1772bf8870dcb0c
SereSant <sere.santurion12@gmail.com>
2024-08-22 23:01:58 -0300

    login html y js

---
+++ js/login.js
@@ -0,0 +1,22 @@
+function redirigirAInicio() {
+    var usuraio = document.getElementById("user").value;
+    var contraseña = document.getElementById("password").value;
+
+    if(usuraio === " " || contraseña === " "){
+        alert("Complete los campos");
+    } 
+    else{
+        localStorage.setItem("isLoggedIn", "true");
+        window.location.href = "index.html";
+    }
+}
+
+function verificarSesion(){
+    if(localStorage.getItem("isLoggedIn") === "true"){
+        window.location.href = "index.html";
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    verificarSesion()
+})
