43b317bdca8ef6a963a0ae8c659720760210efa8
antonella32 <mariatrucillo5@gmail.com>
2024-09-14 21:16:21 -0300

    correciones2

--- js/products.js
+++ js/products.js
@@ -1,27 +1,26 @@
-function getAutos(url){
-    return fetch(url)
-    .then(respuesta => {
-        return respuesta.json();
-    })
-    .then(lista => {
-        showAutosTable(lista.products);
-    })
-}
 
-function showAutosTable(autosList){
+function showAutosTable(autosList) {
     let productsHtml = "";
-    for(let p of autosList){
+    for (let p of autosList) {
         productsHtml += `<tr>
-                          <td><img src=${p.image}></td>
+                          <td><img src="${p.image}" alt="Imagen de ${p.name}" width="100"></td>
                           <td>${p.name}</td>
                           <td>${p.description}</td>
-                          <td>${p.cost}</td>
+                          <td>${p.currency} ${p.cost}</td>
                           <td>${p.soldCount}</td>
                        </tr>`;
     }
     document.getElementById("tautos").innerHTML = productsHtml;
 }
 
-document.addEventListener("DOMContentLoaded",function(){
-    getAutos("https://japceibal.github.io/emercado-api/cats_products/101.json");
+document.addEventListener("DOMContentLoaded", function() {
+    getJSONData("https://japceibal.github.io/emercado-api/cats_products/101.json")
+        .then(function(result) {
+            if (result.status === "ok") {
+                // Llamas a la función para mostrar los autos en la tabla
+                showAutosTable(result.data.products); // Asegúrate de acceder a la propiedad "products"
+            } else {
+                console.error("Error al obtener los datos", result.data);
+            }
+        });
 });
