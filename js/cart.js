8c3e48275c1bffae41d9b3bc72668f600dc3d34e
antonella32 <mariatrucillo5@gmail.com>
2024-10-31 14:59:36 -0300

    algo de cart y css

--- js/cart.js
+++ js/cart.js
@@ -1,4 +1,4 @@
-// Obtener el carrito desde localStorage
+
 let cartItemsContainer = document.getElementById("cart-items");
 
 // Limpiar el contenedor en caso de que ya tenga contenido
@@ -18,7 +18,7 @@ function actualizarCantidad(nombreProducto, nuevaCantidad) {
 
 // Función para mostrar el subtotal del producto
 function mostrarSubtotal(producto) {
-    let subtotalElement = document.querySelector(`.subtotal[data-nombre="${producto.nombre}"]`);
+    let subtotalElement = document.querySelector(`.incart_subtotal[data-nombre="${producto.nombre}"]`);
     if (subtotalElement) {
         let subtotal = producto.precio * producto.cantidad;
         subtotalElement.innerText = `Subtotal: ${subtotal} ${producto.moneda}`;
@@ -30,15 +30,20 @@ listaCarrito.forEach(Originalproduct => {
     let subtotal = Originalproduct.precio * Originalproduct.cantidad; // Calcular subtotal
 
     let item = `
-        <div class="cart-item">
-            <div><h5>${Originalproduct.nombre}</h5></div>
-            <div>Costo: ${Originalproduct.precio} ${Originalproduct.moneda}</div>
-            <div>
-                Cantidad: 
-                <input type="number" value="${Originalproduct.cantidad}" min="1" onchange="actualizarCantidad('${Originalproduct.nombre}', this.value)">
+       <div class="cart-item">
+            <div class="incart_image">
+                <img src="${Originalproduct.imagen}" alt="${Originalproduct.nombre}" />
             </div>
-            <div><img src="${Originalproduct.imagen}" alt="${Originalproduct.nombre}" /></div>
-            <div class="subtotal" data-nombre="${Originalproduct.nombre}">Subtotal: ${subtotal} ${Originalproduct.moneda}</div>
+            <div class="incart_details">
+                <h5 class="incart_name">${Originalproduct.nombre}</h5>
+                <div class="incart_cost">Costo: ${Originalproduct.precio} ${Originalproduct.moneda}</div>
+            </div>
+            <div class="incart_soldcount">
+                Cantidad: 
+                <input type="number" value="${Originalproduct.cantidad}" min="1" 
+                onchange="actualizarCantidad('${Originalproduct.nombre}', this.value)">
+                <div class="incart_subtotal subtotal" data-nombre="${Originalproduct.nombre}">Subtotal: ${subtotal} ${Originalproduct.moneda}</div>
+            </div>
         </div>
     `;
     cartItemsContainer.innerHTML += item; // Agregar el elemento al contenedor
