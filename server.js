0913ab604ca51dce9ac118d7804978330ffb96f8
antonella32 <mariatrucillo5@gmail.com>
2024-12-01 13:52:24 -0300

    entrega8 sin desafiate

--- server.js
+++ server.js
@@ -1,35 +1,37 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const cors = require('cors');  
+const cors = require('cors');  //EL CORS
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const app = express();
+const mysql = require('mysql2'); //sql para el desafiate
 const PORT = 3000;
 
-app.use(cors()); // Permite CORS en todas las rutas
+app.use(cors()); //se usa cors en todas las rutas
 app.use(express.json());
 
-//parte TRESSSSSSSSSSSSSSSSSSSS
+//parte TREs!!!!!!!!!!!!!
+//usuario para probar el endpoint post en postman
 const users = [
-    { username: 'user', password: 'pass' }
+    { username: 'name', password: 'password' }
   ];
   
 
-  // Ruta para autenticar usuarios (POST /login)
 // Ruta POST para login
 app.post('/login', (req, res) => {
     const { username, password } = req.body;
   
-    // Verificar si el usuario existe
+    //se verifica que el usuario existe
     const user = users.find(u => u.username === username && u.password === password);
   
+    //si el usuario existe se crea un token
     if (user) {
-      // Crear un token
+    
 
      //ACA SE GENERA EL TOKEN
       const token = jwt.sign({ username: user.username }, 'secreto', { expiresIn: '1h' });
-      res.json({ token }); // Devolver el token al cliente
+      res.json({ token }); //devuelve el token
     } else {
       res.status(401).json({ message: 'Credenciales inválidas' }); // Si no se encuentran las credenciales
     }
@@ -37,48 +39,52 @@ app.post('/login', (req, res) => {
 
 
 
-//PARTE TRES FIN
+//ACA TERMINA LA PARTE 3!!!!!!!!!!!!!!!!
 
-//PARTE 4
+//!!!!!!!!!!!!!!!!!PARTE 4!!!!!!!!!!!!!!!!!!!!
+
 // Middleware de autorización
-function verificarToken(req, res, next) {
-  // Obtener el token del encabezado 'Authorization'
-  const token = req.headers['authorization'];
+function verificarToken(req, res, next) {  //funcion verificartoken
+  //en postman en authorization se pone el token generado en post login 
+  const token = req.headers['authorization']; //aqui toma el token 
 
   if (!token) {
     return res.status(401).json({ message: 'Acceso denegado. Token no proporcionado.' });
-  }
+  }  //si el token no corresponde da acceso denegado
 
-  // Eliminar el prefijo "Bearer " del token, si está presente
+  //si el token se genera con la Bearer se reemplaza por vacio asi solo queda el token
   const tokenSinBearer = token.replace('Bearer ', '');
 
-  // Verificar y decodificar el token
+
+  //se verifica y decodifica el token
   jwt.verify(tokenSinBearer, 'secreto', (err, decoded) => {
     if (err) {
       return res.status(403).json({ message: 'Token inválido o expirado.' });
     }
 
-    // Almacenar el usuario decodificado para usarlo en las rutas siguientes
+    //se almacena el usuario decodificado apra usarlo ne las rutas siguientes
     req.user = decoded;
 
-    // Continuar con la ejecución de la siguiente función de la ruta
+    // se continua con la ejecución
     next();
   });
 }
 
 module.exports = verificarToken;
 
-//
+
 
 // Middleware para servir archivos estáticos desde la carpeta emercado-api-main
 app.use('/api', express.static(path.join(__dirname, 'emercado-api-main')));
 
-// Ruta de prueba
+//Ruta para probar en postman
 app.get('/', (req, res) => {
   res.send('Bienvenido a la API del e-commerce');
 });
 
-// Ruta para obtener todas las categorías (cat.json) - Protegida
+//------------------------------------------------------------------
+
+//Ruta para obtener la categoría, esta ruta esta protegida con verificar token
 app.get('/api/cats', verificarToken, (req, res) => {
   const catsPath = path.join(__dirname, 'emercado-api-main', 'cats', 'cat.json');
   
@@ -89,32 +95,51 @@ app.get('/api/cats', verificarToken, (req, res) => {
     res.status(404).send('Archivo de categorías no encontrado');
   }
 });
+//------------------------------------------------------------------
+//------------------------------------------------------------------
 
-// Ruta para obtener un producto específico usando su ID (en products) - Protegida
+//Ruta para obtener la categoría, esta ruta esta protegida con verificar token
+app.get('/api/cats_products/:catID', verificarToken, (req, res) => {
+  const catID = req.params.catID;
+  const catsproductsPath = path.join(__dirname, 'emercado-api-main', 'cats_products', `${catID}.json`);
+  
+  // Verificar si el archivo existe
+  if (fs.existsSync(catsproductsPath)) {
+    res.sendFile(catsproductsPath); //devuelve json de cats
+  } else {
+    res.status(404).send('Archivo de categorías no encontrado');
+  }
+});
+//------------------------------------------------------------------
+
+
+//ruta para obtener un producto usando su ID, protegida 
 app.get('/api/products/:productId', verificarToken, (req, res) => {
   const productId = req.params.productId; // ID de producto
-  const productPath = path.join(__dirname, 'emercado-api-main', 'products', `${productId}.json`);
+  const productPath = path.join(__dirname, 'emercado-api-main', 'products', `${productId}.json`); //no es estatica
 
-  // Verificar si el archivo existe
+  //verifica que el archivo existe
   if (fs.existsSync(productPath)) {
-    res.sendFile(productPath); // Devolver el archivo JSON del producto
+    res.sendFile(productPath); //devuelve el json
   } else {
     res.status(404).send('Producto no encontrado');
   }
 });
+//------------------------------------------------------------------
 
-// Ruta para obtener los comentarios de un producto específico - Protegida
+// Ruta para obtener los comentarios de un producto con su ID, protegida
 app.get('/api/products_comments/:productId', verificarToken, (req, res) => {
   const productId = req.params.productId; // ID del producto
   const commentsPath = path.join(__dirname, 'emercado-api-main', 'products_comments', `${productId}.json`);
 
-  // Verificar si el archivo de comentarios existe
+  //verifica que exista el archivo aunque ya sabemos que si está esta bien implementarlo
   if (fs.existsSync(commentsPath)) {
-    res.sendFile(commentsPath); // Devolver el archivo JSON de los comentarios
+    res.sendFile(commentsPath); //devuelve el json
   } else {
     res.status(404).send('Comentarios no encontrados');
   }
 });
+//------------------------------------------------------------------
 
 // Iniciar el servidor
 app.listen(PORT, () => {
